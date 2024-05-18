5aa24367ed4f9798b3bbd4a43327fd4615529db3
Anay Gupta <100482615+Anay0305@users.noreply.github.com>
2024-05-18 10:28:17 +0530

    Update statistics.py

--- cogs/statistics.py
+++ cogs/statistics.py
@@ -144,9 +144,9 @@ class Statistics(commands.Cog):
                     else:
                         day_db[today]['channels'][before.channel.id] = channel_time
             if check_bl_channel(after.channel):
+                await self.bot.main_owner(user_start)
                 if member.id in user_start:
                     user_time = round(datetime.now().timestamp() - user_start[guild.id][member.id])
-                    await self.bot.main_owner(user_time)
                     del user_start[guild.id][member.id]
                     if member.id in user_db:
                         user_db[member.id] += user_time
