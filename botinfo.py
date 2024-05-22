4989b9e7ea1854fb7cd1bd16b8855a85c447e251
Anay Gupta <100482615+Anay0305@users.noreply.github.com>
2024-05-22 09:45:12 +0530

    Update botinfo.py

--- botinfo.py
+++ botinfo.py
@@ -6,7 +6,7 @@ root_color = 0x00ecc6
 wrong_color = 0xff0000
 right_color = 0x00f7ff
 
-webhook_join_leave_logs = "https://discord.com/api/webhooks/1241606976686002186/-n8bizFt09kkRljk9ZeOwlWe-4AloGoes9oPH6J0LL49RaukTNC12MvIh0jknu5RR4P"
+webhook_join_leave_logs = "https://discord.com/api/webhooks/1241606976686002186/_-n8bizFt09kkRljk9ZeOwlWe-4AloGoes9oPH6J0LL49RaukTNC12MvIh0jknu5RR4P"
 webhook_cmd_logs = "https://discord.com/api/webhooks/1241607314113560576/FZydohXtkgkbO8PSXF7i-1MXG5YHD-WvgjVjsMsLS8xjIXPvn4UJQPe18o6lQsi99Bqf"
 webhook_imp_logs = "https://discord.com/api/webhooks/1241607596494950494/Xq50TvTkO50eW_r7VUPbmPNtg8UO1v9fda03krXMVTT08Uo0d8nOPl-NH7Qt9wGfdtHQ"
 webhook_shard_logs = "https://discord.com/api/webhooks/1241607839886344192/OErKudjqwonRSFJJgUl8SSH6W3k9SGR990nYNeHaiQsN8XhhT5tvM-qXri_eejCJgQtJ"
