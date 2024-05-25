63c37feff470e86677dcc677f7d6be2f97d1cc7e
Anay0305 <anayg0305@gmail.com>
2024-05-26 00:10:04 +0530

    s

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -744,10 +744,10 @@ class moderation(commands.Cog):
         if url is not None and name is None:
             name = url
             url = None
-        if ctx.message.reference is None and len(ctx.message.attachments) == 0:
+        if ctx.message.reference is None and len(ctx.message.attachments) == 0 and url is None and not name.startswith("https://"):
             return await ctx.reply("No replied message found")
         msg = await ctx.channel.fetch_message(ctx.message.reference.message_id)
-        if len(msg.stickers) == 0 and len(msg.attachments) == 0 and len(ctx.message.attachments) == 0 and not msg.content.startswith("https://") and url is None:
+        if len(msg.stickers) == 0 and len(msg.attachments) == 0 and len(ctx.message.attachments) == 0 and not msg.content.startswith("https://") and url is None and not name.startswith("https://"):
             return await ctx.reply("No sticker found")
         n, url = "", ""
         if len(msg.stickers) != 0:
