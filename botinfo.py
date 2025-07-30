ee16f9e3470679b90d93da9d9b3d6ac2dc48acc6
Anay Gupta <100482615+Anay0305@users.noreply.github.com>
2025-07-31 03:40:28 +0530

    Update botinfo.py

--- botinfo.py
+++ botinfo.py
@@ -1,33 +1,32 @@
-main_devs = [1141685323299045517, 979353019235840000, 933738517845118976, 966230921084796999, 1096997719635140628, 1244892271866937344]
+main_devs = []
 workowner = main_devs + []
 
-bot_id = 880765863953858601
+bot_id = 0
 dbltoken = ""
-topgg_link = "https://top.gg/bot/880765863953858601/vote"
-support_server = "https://discord.gg/K4v4aEuwp6"
-api_url = "http://159.203.133.30:5000/"
+topgg_link = ""
+support_server = ""
+api_url = ""
 
-wavelink_uri = "http://165.22.45.187:2333/"
-wavelink_pass = "anaygupta"
+wavelink_uri = ""
+wavelink_pass = ""
 
 root_color = 0x00ecc6
 wrong_color = 0xff0000
 right_color = 0x00f7ff
 
-webhook_join_leave_logs = "https://discord.com/api/webhooks/1241606976686002186/_-n8bizFt09kkRljk9ZeOwlWe-4AloGoes9oPH6J0LL49RaukTNC12MvIh0jknu5RR4P"
-webhook_cmd_logs = "https://discord.com/api/webhooks/1241607314113560576/FZydohXtkgkbO8PSXF7i-1MXG5YHD-WvgjVjsMsLS8xjIXPvn4UJQPe18o6lQsi99Bqf"
-webhook_imp_logs = "https://discord.com/api/webhooks/1241607596494950494/Xq50TvTkO50eW_r7VUPbmPNtg8UO1v9fda03krXMVTT08Uo0d8nOPl-NH7Qt9wGfdtHQ"
-webhook_shard_logs ="https://discord.com/api/webhooks/1241607839886344192/OErKudjqwonRSFJJgUl8SSH6W3k9SGR990nYNeHaiQsN8XhhT5tvM-qXri_eejCJgQtJ"
-webhook_rate_limit_logs = "https://discord.com/api/webhooks/1241607839886344192/OErKudjqwonRSFJJgUl8SSH6W3k9SGR990nYNeHaiQsN8XhhT5tvM-qXri_eejCJgQtJ"
-webhook_dm_logs = "https://discord.com/api/webhooks/1215241922508095499/8i79m0Y2KMvlvzo-T-xCvt0np7q8IXtwnhW3GH7Mj5daIjk_vq5CZOG47YljZY6KdfoC"
-webhook_badge_logs = "https://discord.com/api/webhooks/1241608493337935982/IrzvOjWzOhjjJJyQulGIhmwMKiuOBWHZQkCSyi9NvwcOrgsozWfV0L4_GBlPbA7QA1gi"
-webhook_np_logs = "https://discord.com/api/webhooks/1241608928081481728/wup7f3aQglbrRXyPubgGufSr2KyxM_cg9rYEUkLYdN-5o6UZYsbGOGbNItyeeDTMOaPT"
-webhook_blacklist_logs = "https://discord.com/api/webhooks/1242872845277855784/27mBUroHlZcY-m9Vc5wTX6W34s9vRBGxEzkgUFJJ3ucrR2BBTyKZ1qf2D8Di8ixvfSXi"
+webhook_join_leave_logs = ""
+webhook_cmd_logs = ""
+webhook_imp_logs = ""
+webhook_shard_logs = ""
+webhook_rate_limit_logs = ""
+webhook_dm_logs = ""
+webhook_badge_logs = ""
+webhook_np_logs = ""
+webhook_blacklist_logs = ""
 
 starttime = 0
 rate_limit = {}
 warning_count = {}
 
-sputnik_token = "MTI0MDAwNTYwMTIyMDc1NTU1Nw.Ghp7NV.jSO1uHw1d6TlCeV526RLEYaUM9-KxtNTDs-o58"
-gateway_token = "ODgwNzY1ODYzOTUzODU4NjAx.GIkKnz.aY_wX8Hqq3WNVQJ2D3coM9aoRUb8tI7A7ODX7I"
-slut_token = "MTIxNTM3NjQzMDYyMDM1MjUxMw.GEM4H3.me5UPCr8PYFecjzPDFKApr0CQtN-ysf9BGKgvs"
+sputnik_token = ""
+gateway_token = ""
